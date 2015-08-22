34486f988f20b3b997496c48cfed1e865bcc5065
Christopher Kintner <ckintner@zendesk.com>
2015-08-22 14:00:20 -0700

    remove div when clicked

--- injected.js
+++ injected.js
@@ -7,10 +7,18 @@ chrome.runtime.onMessage.addListener(
 
 function photonAddHostLabel(hostName) {
   var div = document.createElement("div");
-  div.id = "photon-host"
+  div.id = "photon-host";
   div.innerHTML = hostName;
   document.body.appendChild(div);
   window.setTimeout(function(){
     div.style.opacity = 1;
   });
+
+  div.addEventListener("click", function() {
+    div.style.opacity = 0;
+    window.setTimeout(function() {
+      div.parentNode.removeChild(div);
+    }, 1);
+  });
+
 }
